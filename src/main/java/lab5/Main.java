77228f3b2777a3d554472bd8914e721ce4926015
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 22:45:52 +0300

    Scripted auto-commit on change (2020-11-22 22:45:52) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -73,7 +73,7 @@ public class Main {
                         if (res != null) {
                             return new CompletedFuture<Integer>((Integer) res, null);
                         }
-                        
+                        Sink<>
                         Graph<SinkShape<Pair<String, Integer>>, CompletionStage<HttpResponse>> testSink =
                                 Flow.<Pair<String, Integer>>create()
                                 .mapConcat(pair->{
