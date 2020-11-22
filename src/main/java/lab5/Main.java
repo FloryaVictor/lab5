55ba1863ab67d986a1110927c344350076f679e8
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:11:26 +0300

    Scripted auto-commit on change (2020-11-22 18:11:26) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -1,6 +1,6 @@
 package lab5;
 
 public class Main {
-    ActorSystem system = ActorSystem.create(responsive) "simplest-ом в ответ на запрос события test");
-
+    ActorSystem system = ActorSystem.create("lab5");
+    
 }
