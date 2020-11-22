74a53e50bc0f3b965a0c4ce6f0c494534cb2e5fd
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:41:38 +0300

    Scripted auto-commit on change (2020-11-22 18:41:37) by gitwatch.sh

--- src/main/java/lab5/Actors/CacheActor.java
+++ src/main/java/lab5/Actors/CacheActor.java
@@ -1,2 +1,10 @@
-package lab5.Actors;public class CacheActor {
+package lab5.Actors;
+
+import akka.actor.AbstractActor;
+
+public class CacheActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
