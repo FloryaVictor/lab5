6669500e2367a08fc05e76035dbd859dd308ed6d
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 20:34:59 +0300

    Scripted auto-commit on change (2020-11-22 20:34:59) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -65,10 +65,9 @@ public class Main {
                 })
                 .mapAsync(1, (Pair<String, Integer> p)->{
                     CompletionStage<Object> cs = Patterns.ask(cache, new GetMsg(p.first()), timeout);
-                    Function<Integer, CompletableFuture<Integer>> f = (Integer i)->{
-                        return CompletableFuture.runAsync();
-                    };
-                    
+                    cs.thenRun(()->{
+
+                    });
                 });
 
     }
