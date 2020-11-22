3a112ca8385fd8d80b4bafe2a0b43097d0e348f5
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:40:31 +0300

    Scripted auto-commit on change (2020-11-22 18:40:31) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -19,16 +19,16 @@ public class Main {
         ActorSystem system = ActorSystem.create("lab5");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-               routeFlow,
-               ConnectHttp.toHost("localhost", 8080),
-               materializer
-        );
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read();
-        binding
-                .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate());
+//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
+//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+//               routeFlow,
+//               ConnectHttp.toHost("localhost", 8080),
+//               materializer
+//        );
+//        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+//        System.in.read();
+//        binding
+//                .thenCompose(ServerBinding::unbind)
+//                .thenAccept(unbound -> system.terminate());
     }
 }
