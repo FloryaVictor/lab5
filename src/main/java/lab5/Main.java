76f2583623918344c5b259e88c7efad48df1e8d3
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:11:34 +0300

    Scripted auto-commit on change (2020-11-22 18:11:34) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -2,5 +2,5 @@ package lab5;
 
 public class Main {
     ActorSystem system = ActorSystem.create("lab5");
-    
+    ActorMaterializer materializer = ActorMaterializer.create(responsive) system);
 }
