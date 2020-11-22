4e584984d89dd604348e0176a2be05cfdb90d6e7
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:52:59 +0300

    Scripted auto-commit on change (2020-11-22 18:52:59) by gitwatch.sh

--- src/main/java/lab5/Messages/GetMsg.java
+++ src/main/java/lab5/Messages/GetMsg.java
@@ -1,2 +1,18 @@
-package lab5.Messages;public class GetMsg {
+package lab5.Messages;
+
+public class GetMsg {
+    private final String url;
+    private final Integer time;
+    public GetMsg(String url, Integer time){
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
 }
--- src/main/java/lab5/Messages/StoreMsg.java
+++ src/main/java/lab5/Messages/StoreMsg.java
@@ -1,2 +1,18 @@
-package lab5.Messages;public class StoreMsg {
+package lab5.Messages;
+
+public class StoreMsg {
+    private final String url;
+    private final Integer time;
+    public StoreMsg(String url, Integer time){
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
 }
