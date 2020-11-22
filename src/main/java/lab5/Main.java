aecb7b935bba41261bba3b71da61027452a75e40
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:11:51 +0300

    Scripted auto-commit on change (2020-11-22 18:11:51) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -1,6 +1,10 @@
 package lab5;
 
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+
 public class Main {
     ActorSystem system = ActorSystem.create("lab5");
-    ActorMaterializer materializer = ActorMaterializer.create(responsive) system);
+    ActorMaterializer materializer = ActorMaterializer.create(system);
+    
 }
