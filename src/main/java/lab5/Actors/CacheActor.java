97771cde2f84a12f615d6566ff7ed4f851619028
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 18:50:54 +0300

    Scripted auto-commit on change (2020-11-22 18:50:53) by gitwatch.sh

--- src/main/java/lab5/Actors/CacheActor.java
+++ src/main/java/lab5/Actors/CacheActor.java
@@ -1,11 +1,20 @@
 package lab5.Actors;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
 
 public class CacheActor extends AbstractActor {
-    
+    private final HashMap<String, Integer> cache = new HashMap<>();
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(String.class, url->{
+                    getSender().tell(cache.getOrDefault(url, null), ActorRef.noSender());
+                })
+                .build();
     }
 }
