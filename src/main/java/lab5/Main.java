2837efa615fa69a507ae1b1c131d7e0baeacea1e
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 20:57:01 +0300

    Scripted auto-commit on change (2020-11-22 20:57:01) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -66,6 +66,7 @@ public class Main {
                         Graph<SinkShape<Pair<String, Integer>>, CompletionStage<HttpResponse>> teskSink =
                                 Flow.<Pair<String, Integer>>create()
                                 .mapConcat()
+                                
                         return Source.single(p)
                                 .toMat(teskSink, Keep.right()).run(mat);
                     });
