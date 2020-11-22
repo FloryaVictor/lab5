b70b068177d562af9252b0791449e2d23686b779
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 20:25:12 +0300

    Scripted auto-commit on change (2020-11-22 20:25:12) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -62,8 +62,8 @@ public class Main {
                 })
                 .mapAsync(1, (Pair<String, Integer> p)->{
                     CompletionStage<Object> cs = Patterns.ask(cache, new GetMsg(p.first()), timeout);
-                    cs.thenCompose((Integer time) ->{
-                        return new CompletedFuture<Integer>((Integer) time, null);
+                    cs.thenCompose((Object time) ->{
+                        return new CompletedFuture<Object>((Integer) time, null);
                     });
                 });
 
