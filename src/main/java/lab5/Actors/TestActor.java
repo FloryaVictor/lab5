e4876020bd32fb21177ad43b7018a3f56915a9e9
FloryaVictor <lapunixa@yandex.ru>
2020-11-22 19:04:17 +0300

    Scripted auto-commit on change (2020-11-22 19:04:17) by gitwatch.sh

--- src/main/java/lab5/Actors/TestActor.java
+++ src/main/java/lab5/Actors/TestActor.java
@@ -4,6 +4,8 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
+import lab5.Messages.GetMsg;
+import lab5.Messages.TestMsg;
 
 public class TestActor extends AbstractActor {
     private final ActorRef cacheActor = getContext().actorOf(
@@ -13,6 +15,12 @@ public class TestActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match()
+                .match(TestMsg.class, msg->{
+                    
+                })
+                .match(GetMsg.class, msg->{
+
+                })
+                .build();
     }
 }
